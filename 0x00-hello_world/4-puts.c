88422e88007c72fb91c6de4d9604ad36e7776f93
Psam604 <eguacity4christ@gmail.com>
2023-09-07 09:06:09 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /**
  * main - this is the main function
  *
  * Return: always o
- */
+*/
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle):
-	return (o):
+	puts("\"Programming is like building a multilingual puzzle");
+	return (o);
 }
