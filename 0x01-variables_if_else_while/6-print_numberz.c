f44e08eb335dff21226aba0ee6f88c62ab706e57
Psam604 <eguacity4christ@gmail.com>
2023-09-10 13:52:51 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - program execution starts
+ *
+ * Return: 0 (successful)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar('0' + num);
+	}
+	putchar('\n');
+
+	return (0);
+}
