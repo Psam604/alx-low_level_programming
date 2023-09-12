3393cc3d80b2d980c9c7b6e32adf0b79ec4c02c1
Psam604 <eguacity4christ@gmail.com>
2023-09-12 15:40:30 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - A function that print_alphabet
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+
+	_putchar('\n');
+}
