09b4f5c052f7016f67ba91b590152521b02fc9de
Psam604 <eguacity4christ@gmail.com>
2023-09-12 17:56:28 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - print the absolute value of numbers
+ * @n: number to be evaluated
+ *
+ * Return: absolute value of n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
