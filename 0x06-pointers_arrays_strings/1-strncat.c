7ced93c73db0f7631bc61aa149342c567cbb6c13
Psam604 <eguacity4christ@gmail.com>
2023-09-20 03:36:50 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * strncat - function that concatanates two string
+ * @dest: pointer to destination string
+ * @src: pointer to source string
+ * @n: number of byte to be concatenated
+ *
+ * Return: pointer to destination string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+
+{
+	int length, j;
+/* j is a counter for n byte of src to be concatenated */
+/* length = length of destination string */
+
+	length = 0;
+	while (dest[length] != '\0')
+	{
+		length++;
+	}
+	for (j = 0; j < n && src[j] != '\0'; j++, length++)
+	{
+		dest[length] = src[j];
+	}
+	dest[length] = '\0';
+	return (dest);
+}
